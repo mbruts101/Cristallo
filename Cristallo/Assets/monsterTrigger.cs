cda7aadfb73a288de70e25f5188b9edf95faaedb
sdcole96 <DCole@Daniels-MacBook-Pro-15.local>
2017-04-27 18:13:14 -0700

    tinkered with enemies. Readded tags

--- Cristallo/Assets/monsterTrigger.cs
+++ Cristallo/Assets/monsterTrigger.cs
@@ -4,18 +4,21 @@ using System.Collections;
 public class monsterTrigger : MonoBehaviour
 {
 	private int t;
+	public int distance;
 	public int speed;
+	public int c;
 	// Use this for initialization
 	void Start () 
 	{
 		t = 0;
+		distance = distance + 1;
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
 		this.transform.Translate(Vector3.right * speed * Time.deltaTime);
-		if (t % 150 == 0) 
+		if ((t - c) % distance == 0) 
 		{
 			speed = -speed;
 		}
