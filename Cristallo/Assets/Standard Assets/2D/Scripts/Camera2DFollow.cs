37cf62413060874069273cf0948e241de8b54c05
sdcole96 <DCole@Daniels-MacBook-Pro-15.local>
2017-05-02 01:39:01 -0700

    Level 1 Skeleton Design complete

--- Cristallo/Assets/Standard Assets/2D/Scripts/Camera2DFollow.cs
+++ Cristallo/Assets/Standard Assets/2D/Scripts/Camera2DFollow.cs
@@ -10,6 +10,7 @@ namespace UnityStandardAssets._2D
         public float lookAheadFactor = 3;
         public float lookAheadReturnSpeed = 0.5f;
         public float lookAheadMoveThreshold = 0.1f;
+		public bool titleScreen = true;
 
         private float m_OffsetZ;
         private Vector3 m_LastTargetPosition;
@@ -24,30 +25,34 @@ namespace UnityStandardAssets._2D
             transform.parent = null;
         }
 
+		void changeCamera()
+		{
+			titleScreen = false;
+		}
 
         // Update is called once per frame
         private void Update()
         {
             // only update lookahead pos if accelerating or changed direction
-            float xMoveDelta = (target.position - m_LastTargetPosition).x;
+			if(!titleScreen)
+			{
+				float xMoveDelta = (target.position - m_LastTargetPosition).x;
 
-            bool updateLookAheadTarget = Mathf.Abs(xMoveDelta) > lookAheadMoveThreshold;
+				bool updateLookAheadTarget = Mathf.Abs (xMoveDelta) > lookAheadMoveThreshold;
 
-            if (updateLookAheadTarget)
-            {
-                m_LookAheadPos = lookAheadFactor*Vector3.right*Mathf.Sign(xMoveDelta);
-            }
-            else
-            {
-                m_LookAheadPos = Vector3.MoveTowards(m_LookAheadPos, Vector3.zero, Time.deltaTime*lookAheadReturnSpeed);
-            }
+				if (updateLookAheadTarget) {
+					m_LookAheadPos = lookAheadFactor * Vector3.right * Mathf.Sign (xMoveDelta);
+				} else {
+					m_LookAheadPos = Vector3.MoveTowards (m_LookAheadPos, Vector3.zero, Time.deltaTime * lookAheadReturnSpeed);
+				}
 
-            Vector3 aheadTargetPos = target.position + m_LookAheadPos + Vector3.forward*m_OffsetZ;
-            Vector3 newPos = Vector3.SmoothDamp(transform.position, aheadTargetPos, ref m_CurrentVelocity, damping);
+				Vector3 aheadTargetPos = target.position + m_LookAheadPos + Vector3.forward * m_OffsetZ;
+				Vector3 newPos = Vector3.SmoothDamp (transform.position, aheadTargetPos, ref m_CurrentVelocity, damping);
 
-            transform.position = newPos;
+				transform.position = newPos;
 
-            m_LastTargetPosition = target.position;
+				m_LastTargetPosition = target.position;
+			}
         }
     }
 }
