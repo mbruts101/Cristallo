f132dc102db18d64038f3a5ce63fa2aa5a7eab60
sdcole96 <DCole@Daniels-MacBook-Pro-15.local>
2017-05-04 14:25:13 -0700

    Level 1.

---
+++ Cristallo/Assets/prompt.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class prompt : MonoBehaviour {
+	public Sprite s1;
+	public Sprite s2;
+	private SpriteRenderer sr;
+	// Use this for initialization
+	void Start () 
+	{
+		sr = gameObject.GetComponent<SpriteRenderer>(); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		sr.sprite = s1;
+	}
+
+	void OnTriggerExit2D(Collider2D other)
+	{
+		sr.sprite = s2;
+	}
+}
